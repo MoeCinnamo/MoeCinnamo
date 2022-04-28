87c8cd7b6193ecb8331d1ad66f9da5f505a5dc33
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-04-28 17:48:27 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -20,7 +20,7 @@ export const github = {
 
 export const mxSpace = {
   url: 'https://blog.mojy.xyz',
-  api: 'https://api.innei.ren/v2',
+  api: 'https://blog.mojy.xyz/api',
 }
 
 export const timeZone = 'Asia/Shanghai'
