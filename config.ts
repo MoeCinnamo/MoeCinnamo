be5d329a447a8f37d3ab5466b05cb5cc5dec394d
喵千寻 <101342651+Cinnamoroll-Rabbit@users.noreply.github.com>
2022-04-28 16:21:19 +0800

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,25 @@
+export const opensource = {
+  active: [
+    'Cinnamoroll-Home/ShortUrl-Static',
+    'Cinnamoroll-Home/ShortUrl-PHP',
+    'Cinnamoroll-Home/CloudDisk-Static-Template',
+  ],
+  toys: {
+    limit: 3,
+    random: true,
+    repos: [
+      //'',
+    ],
+  },
+}
+export const motto = `<p align=center><strong>大耳犬は一番かわいいです。ニャンコが萌えます。</strong></p>`
+export const github = {
+  name: 'Cinnamoroll-Rabbit',
+}
+
+export const mxSpace = {
+  url: 'https://innei.ren',
+  api: 'https://api.innei.ren/v2',
+}
+
+export const timeZone = 'Asia/Shanghai'
